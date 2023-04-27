745ed88aa4e190bbc3ad486526b1e2cabfd08746
Luisa Kirchhof <luisa.kirchhof@kellogg.northwestern.edu>
2023-04-27 00:15:37 +0000

    created company and contacts tables

---
+++ app/models/company.rb
@@ -0,0 +1,3 @@
+class Company < ApplicationRecord
+    # talks to the companies table
+end
---
+++ db/migrate/20230426235219_create_companies.rb
@@ -0,0 +1,14 @@
+class CreateCompanies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :companies do |t|
+
+      # t.integer "id" - created automatically
+      t.string "name"
+      t.string "city"
+      t.string "state"
+      t.string "url"
+
+      t.timestamps
+    end
+  end
+end
